edee572098c36c12dcdfa9ffb994d58a05cc9f29
Sam Lathrop <samuellathrop@users.noreply.github.com>
2026-02-13 17:17:43 -0600

    Commit

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,7 @@
+
+
+SELECT *
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+LIMIT 5 
